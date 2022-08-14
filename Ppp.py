b9f9821af1d4328c93d388b7929034f549b895e4
ADMinOKMisha <103426989+ADMinOKMisha@users.noreply.github.com>
2022-08-14 22:46:07 +0300

    Create Ppp.py

---
+++ Ppp.py
@@ -0,0 +1,10 @@
+import os 
+os.system ("clear") 
+print("                _______________________________")
+y=input("               |") 
+print("                _______________________________") 
+x= y[:4]
+if x==("когда"):
+  os.system("cd otvet && python ccc.py") 
+else:
+  os.system("cd otvet && python ttt.py")
